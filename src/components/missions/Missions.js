b5480b45a39a21e5186d69d343644c7caa589f84
Vanessa O <vaneolipa@gmail.com>
2022-12-20 16:42:00 -0500

    Dispatch leave mission, Use the React conditional to Switch badges

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,9 +1,14 @@
-import { useSelector } from 'react-redux';
-import Mission from './Mission';
+import { useSelector, useDispatch } from 'react-redux';
+import { joinMission } from '../../redux/missions/missions';
 import '../../styles/missions.css';
 
 const Missions = () => {
   const state = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  const handleJoin = (id) => {
+    dispatch(joinMission(id));
+  };
 
   return (
     <>
@@ -20,11 +25,44 @@ const Missions = () => {
             </thead>
             <tbody>
               {state.map((mission) => (
-                <Mission
-                  key={mission.id}
-                  name={mission.name}
-                  description={mission.description}
-                />
+                <tr key={mission.id} className="border table-row-body">
+                  <td className="mission-name">{mission.name}</td>
+                  <td className="mission-description">{mission.description}</td>
+                  <td className="mission-badge">
+                    <p
+                      className="badge-not-member"
+                      style={{ display: mission.reserved ? 'none' : 'flex' }}
+                    >
+                      NOT A MEMBER
+                    </p>
+                    <p
+                      className="badge-menber"
+                      style={{ display: mission.reserved ? 'flex' : 'none' }}
+                    >
+                      Active Member
+                    </p>
+                  </td>
+                  <td className="border mission-buttons">
+                    <button
+                      id={mission.id}
+                      className="join-button"
+                      type="button"
+                      onClick={() => handleJoin(mission.id)}
+                      style={{ display: mission.reserved ? 'none' : 'block' }}
+                    >
+                      Join Mission
+                    </button>
+                    <button
+                      id={mission.id}
+                      className="leave-button"
+                      type="button"
+                      onClick={() => handleJoin(mission.id)}
+                      style={{ display: mission.reserved ? 'block' : 'none' }}
+                    >
+                      Leave Mission
+                    </button>
+                  </td>
+                </tr>
               ))}
             </tbody>
           </table>
